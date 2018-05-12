7f89d85dace1693e0e94a3f2b438e841f226f21b
yuriprym <samuel.young.103@gmail.com>
2018-05-12 13:50:52 -0400

    I can pull out the top ten evalue now

--- filtering_script.py
+++ filtering_script.py
@@ -7,20 +7,42 @@ e_val = []
 protein_search=[]
 seq=[]
 inp=sys.argv[1]
-
+query=[]
 with open(inp, 'r') as data:
     for inc in data:
         line= inc.rstrip('\n')
         line= line.lstrip()
-        if (line.startswith('>')):
-            seq_id.append(line)  
-        elif (re.search('0.0[0-4][0-9]*',line)):
-            neoin = re.search('[0-9]e-[0-9]*',line)
-            #neoin = re.match('[0-9]e-[0-9]*',line)
-            print(neoin)
-            print(type(neoin))
+        if line.startswith('Query= lcl'):
+            query.append(line)
+        GU0 = re.match('GU071[0-9]*.1_prot_ADO9[0-9][0-9]*.1.1',line)
+        KJ_or_Y= re.match('K[A-Z]0190[0-9]*.1_prot_A[A-Z][A-X][0-9*.1_[1-4]*',line)
+        NC_0= re.match("NC_0[0-4][0-9]*.1_prot_YP_[0-9]*.1_[0-9]*",line)
+        J_N_or_F=re.match('J[B-P][0-9]7[0-9][0-9]*.1_prot_AF[A-F[0-9]*.1_[0-4]*',line)
+        D_or_HQ= re.match("[D-H]Q[1-9]*.[0-4]_prot_A[A-P][C-Z][0-9*.[0-9]*",line)
+        if GU0:
+            seq_id.append(GU0.group())  
+        elif (KJ_or_Y):
+            seq_id.append(KJ_or_Y.group())
+        elif (NC_0):
+            seq_id.append(NC_0.group())
+        elif (J_N_or_F):
+            seq_id.append(J_N_or_F.group())
+        elif (line.startswith('MH10')):
+            seq_id.append(line)
+        elif (D_or_HQ):
+            seq_id.append(D_or_HQ.group())
+        newline= re.search('0.0[0-4][0-9]*',line)
+        neoin = re.search('[0-9]e-[0-9][0-9]*',line)
+        if neoin:
+            neoin=float(neoin.group())
             e_val.append(neoin)
-            #print(type(line))
-            print(type(e_val))
-            print(e_val)
-           #print(line)
+        elif newline:
+            newline=float(newline.group())
+            e_val.append(newline)
+        else: 
+            seq.append(line)
+
+
+print(query[0])
+print(seq_id[0:10])
+print(e_val[0:10])
