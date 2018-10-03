17523b659ddd133f0c9c11397eac5f64e1ac242b
Young <youngse7@compile.vcu.edu>
2018-10-03 14:08:09 -0400

    add a randomizer

---
+++ randomization.py
@@ -0,0 +1,14 @@
+import random
+
+randomization= random.randint(1,345)
+print(randomization)
+
+sequences= input()
+sequences=int(sequences)
+if sequences == 215:
+  ramdomize= random.randint(1,215)
+elif sequences >= 40 and sequences <= 100: 
+  ramdomize= random.randint(1,100)
+else:
+  ramdomize= random.randint(1,10000)
+print(randomize)
