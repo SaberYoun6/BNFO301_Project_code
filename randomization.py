755afb67caa88c5577a6ca33942924da865f5b6a
yuriprym <samuel.young.103@gmail.com>
2018-11-13 16:58:22 -0500

    made randomize easier to use and read

--- randomization.py
+++ randomization.py
@@ -1,14 +1,9 @@
 import random
 
-randomization= random.randint(1,345)
+randomization= random.randint(1,328)
 print(randomization)
 
 sequences= input()
 sequences=int(sequences)
-if sequences == 215:
-  ramdomize= random.randint(1,215)
-elif sequences >= 40 and sequences <= 100: 
-  ramdomize= random.randint(1,100)
-else:
-  ramdomize= random.randint(1,10000)
-print(randomize)
+ramdomize= random.randint(1,sequences)
+print(ramdomize)
